fea31e6b619cd08cec705714bc3ab0bf926a9bf5
Kenny Andersson <kenny@ordinary.se>
2023-12-01 15:22:14 +0000

    Day 2 part 2 and other things

--- D01/main.cpp
+++ D01/main.cpp
@@ -1,4 +1,5 @@
 #include <cctype>
+#include <cstddef>
 #include <fstream>
 #include <iostream>
 #include <ostream>
@@ -6,6 +7,8 @@
 #include <sys/types.h>
 
 int sum = 0;
+std::string numbers[10] = {"zero", "one", "two",   "three", "four",
+                           "five", "six", "seven", "eight", "nine"};
 // std::string tmpStorage;
 
 void part1(std::string &line) {
@@ -17,7 +20,7 @@ void part1(std::string &line) {
       break;
     }
   }
-  for (u_int32_t i = line.length(); i >= 0; i--) {
+  for (unsigned int i = line.length(); i >= 0; i--) {
     if (isdigit(line[i])) {
       tmpStorage = tmpStorage + line[i];
       break;
@@ -26,13 +29,25 @@ void part1(std::string &line) {
   sum += std::stoi(tmpStorage);
 }
 
+void part2(std::string &line) {
+
+  for (int i = 0; i <= 9; i++) {
+    size_t pos = line.find(numbers[i]);
+    std::cout << pos << std::endl;
+    // if (pos > 0)
+    //   std::cout << pos << std::endl;
+  }
+}
+
 int main(int argc, char *argv[]) {
   std::string line;
 
   std::ifstream file("D01.txt");
   while (getline(file, line)) {
-    part1(line);
+    // part1(line);
+    part2(line);
+    break;
   }
-  std::cout << sum << std::endl;
+  // std::cout << sum << std::endl;
   return 0;
 }
